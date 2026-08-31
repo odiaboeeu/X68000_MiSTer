0c3012cc18e6fbee2da308e8bf04ad389f25aeac
odiaboeeu <nelsoncmjr@outlook.com>
2026-08-30 23:56:27 -0300

    fix(dmac): detect odd base address in chain mode

--- rtl/DMAC/dma1ch.vhd
+++ rtl/DMAC/dma1ch.vhd
@@ -206,6 +206,7 @@ signal PROTECTED_BLOCK:std_logic;
 signal START_TERR     :std_logic;
 signal START_MERR     :std_logic;
 signal START_BERR     :std_logic;
+signal START_BAERR    :std_logic;
 signal LOAD_MERR      :std_logic;
 signal START_CERR     :std_logic;
 signal CNT_TERR       :std_logic;
@@ -269,6 +270,9 @@ START_MERR<='1' when START_EVT='1' and
 START_BERR<='1' when START_EVT='1' and
                      OCR_CHAIN="10" and BTC=x"0000" else '0';
 
+START_BAERR<='1' when START_EVT='1' and
+                      OCR_CHAIN(1)='1' and BAR(0)='1' else '0';
+
 LOAD_MERR<='1' when (MTC_load='1' and b_indatl=x"0000") or
                     (MTC_BTC='1' and BTC=x"0000") else '0';
 
@@ -292,7 +296,7 @@ CNT_CERR<='1' when CNT_EVT='1' and
                    OCR_CHAIN(1)='1' else '0';
 
 DMA_ERROR_EVT<=START_TERR or PROT_WRITE_EVT or CNT_TERR or
-               START_MERR or START_BERR or LOAD_MERR or
+               START_MERR or START_BERR or START_BAERR or LOAD_MERR or
                START_CERR or CNT_CERR;
 
 START_VALID<='1' when START_EVT='1' and DMA_ERROR_EVT='0' else '0';
@@ -301,6 +305,7 @@ DMA_ERROR_CODE<=
         "00010" when START_TERR='1' or PROT_WRITE_EVT='1' or CNT_TERR='1' else
         "01101" when START_MERR='1' or LOAD_MERR='1' else
         "01111" when START_BERR='1' else
+        "00111" when START_BAERR='1' else
         "00001" when START_CERR='1' or CNT_CERR='1' else
         "00000";
 

ad84c62fd2fdeeace06c20817cd4908da3e170a7
odiaboeeu <nelsoncmjr@outlook.com>
2026-08-31 20:16:33 -0300

    fix(dmac): trigger PCL abort on current edge

--- rtl/DMAC/dma1ch.vhd
+++ rtl/DMAC/dma1ch.vhd
@@ -316,8 +316,8 @@ DMA_ERROR_EVT<=START_TERR or PROT_WRITE_EVT or CNT_TERR or
                START_CERR or CNT_CERR or PCL_ABORT_EVT;
 
 PCL_ABORT_EVT<='1' when DCR_PCL="11" and
-                         DCR_DTYPE(0)='0' and S_PCT='1' and
-                         (CCR_STR='1' or CHactive='1') else '0';
+                         DCR_DTYPE(0)='0' and CHactive='1' and
+                         PCLI_PREV='1' and pcli='0' else '0';
 
 START_VALID<='1' when START_EVT='1' and DMA_ERROR_EVT='0' else '0';
 

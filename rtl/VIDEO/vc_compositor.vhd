012b371fcdcb3d39b34eccd17672fea4058b3378
odiaboeeu <nelsoncmjr@outlook.com>
2026-09-07 12:59:31 -0300

    fix(video): restore text palette color 0 priority handling

--- rtl/VIDEO/vc_compositor.vhd
+++ rtl/VIDEO/vc_compositor.vhd
@@ -153,9 +153,14 @@ begin
         end if;
 
 		if spren = '0' and txten = '1' and tx_pal_eff = 0 then
-			rep_source := SRC_TX;
-			rep_color  := tx_color;
-			st_present := tx_color /= x"0000";
+		    rep_source := SRC_TX;
+		    if unsigned(pri_tx) > unsigned(pri_gr) then
+		        rep_color  := tx_color0;
+		        st_present := tx_color0 /= x"0000";
+		    else
+		        rep_color  := (others => '0');
+		        st_present := false;
+		    end if;
 		end if;
 
         if pri_gr = "11" and gr_present then
